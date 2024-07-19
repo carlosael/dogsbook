2acb7071966c9d659fc2d7fc2b0c58b6fab87a99
carlosael <carlosalbertoel@outlook.com>
2024-07-19 09:42:07 -0300

    feat: add UserContext

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import Home from './Components/Home';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Components/Login/Login';
+import { UserStorage } from './UserContext';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login/*' element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login/*' element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
